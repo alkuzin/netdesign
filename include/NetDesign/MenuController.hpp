954380fbf8b9a8cc3e8b3d283780d2500a4ec843
alkuzin <alkuzindev@gmail.com>
2024-12-28 18:46:12 +0300

    feat: added MenuController class

---
+++ include/NetDesign/MenuController.hpp
@@ -0,0 +1,46 @@
+/**
+ * NetDesign - simple network design tool.
+ * Copyright (C) 2024-2025 Alexander (@alkuzin)
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <https://www.gnu.org/licenses/>.
+ */
+
+#ifndef NETD_MENU_CONTROLLER_HPP
+#define NETD_MENU_CONTROLLER_HPP
+
+#include <QtWidgets/QMainWindow>
+#include <QtWidgets/QMenuBar>
+
+
+namespace netd {
+
+class MenuController : public QObject
+{
+    public:
+        MenuController(void) noexcept;
+
+        void onProjectNew(void) noexcept;
+        void onProjectOpen(void) noexcept;
+        void onProjectSave(void) noexcept;
+        void onProjectExit(void) noexcept;
+
+        void onEditLoadFile(void) noexcept;
+
+        void onInfoDocs(void) noexcept;
+        void onInfoAbout(void) noexcept;
+};
+
+} // namespace netd
+
+#endif // NETD_MENU_CONTROLLER_HPP
---
+++ src/controller/MenuController.cpp
@@ -0,0 +1,64 @@
+/**
+ * NetDesign - simple network design tool.
+ * Copyright (C) 2024-2025 Alexander (@alkuzin)
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <https://www.gnu.org/licenses/>.
+ */
+
+#include <NetDesign/MenuController.hpp>
+
+
+namespace netd {
+
+MenuController::MenuController(void) noexcept
+{
+
+}
+
+void MenuController::onProjectNew(void) noexcept
+{
+
+}
+
+void MenuController::onProjectOpen(void) noexcept
+{
+
+}
+
+void MenuController::onProjectSave(void) noexcept
+{
+
+}
+
+void MenuController::onProjectExit(void) noexcept
+{
+
+}
+
+void MenuController::onEditLoadFile(void) noexcept
+{
+
+}
+
+void MenuController::onInfoDocs(void) noexcept
+{
+
+}
+
+void MenuController::onInfoAbout(void) noexcept
+{
+
+}
+
+} // namespace netd
